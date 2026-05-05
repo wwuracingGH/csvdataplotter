d6b53084c9378903298e5f77b681a80000e286e6
Nicole Swierstra <Swiersw@wwu.edu>
2026-05-05 01:36:04 -0700

    first commit

---
+++ plot.py
@@ -0,0 +1,112 @@
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
+import sys, math, os
+
+from os import listdir
+from os.path import isfile, join
+
+def get_points(fp : str, centerpoint) -> tuple[tuple[float, float], list[float], list[float]]:
+    xvals = []
+    yvals = []
+    with open(fp) as f:
+        channels = f.readline().split(",")
+        try:
+            Lat_index = next(i for i,d in enumerate(channels) if d.startswith("\"Latitude"))
+            Lon_index = next(i for i,d in enumerate(channels) if d.startswith("\"Longitude"))
+        except:
+            return None, 0, 0 
+        
+        data = f.readline().split(",")
+        #print(data[Lat_index])
+        while(data[Lat_index] == '' or data[Lat_index] == '0.0'):
+            data = f.readline().split(",")
+            if (len(data) < 10): 
+                return None, 0, 0 
+            #print(data[Lat_index])
+
+
+
+        if (centerpoint is None):
+            lat = float(data[Lat_index])
+            lon = float(data[Lon_index])
+
+            centerpoint = (lat, lon)
+
+        lat, lon = centerpoint
+
+        lat_radians = lat * math.pi / 180.0
+        m_per_deg_lat = 111132.954 - 559.822 * math.cos(2 * lat_radians) + 1.175 * math.cos(4 * lat_radians)
+        m_per_deg_lon = 111412.84 * math.cos(lat_radians) - 93.5 * math.cos(3 * lat_radians)
+        
+
+        while(1):
+            line = f.readline()
+            if line == '': break
+            data = line.split(",")
+
+            if len(data) > 100:
+                if data[Lat_index] != '':
+                    lat_x = (float(data[Lat_index]) - centerpoint[0]) * m_per_deg_lat
+                    lon_x = (float(data[Lon_index]) - centerpoint[1]) * m_per_deg_lon
+                
+                    yvals.append(lat_x)
+                    xvals.append(lon_x)
+        
+    return (centerpoint, xvals, yvals)
+
+
+current_file = 1
+
+if __name__ == '__main__':
+    fp = sys.argv[1]
+    filedat = []
+    if (os.path.isfile(fp)):
+        centerpoint, xp, yp = get_points(fp, None)
+        filedat.append([xp, yp]) 
+    elif (os.path.isdir(fp)):
+        # from stackoverflow lol
+        logs = [f for f in listdir(fp) if (isfile(join(fp, f)) and f.endswith(".log"))]
+        centerpoint = None
+        for f in logs:
+            print("reading " + join(fp, f))
+            centerpoint, xp, yp = get_points(join(fp, f), centerpoint)
+            if (centerpoint == None or len(xp) == 0):
+                print("Error reading file " + str(f))
+            else: 
+                filedat.append([f, xp, yp]) 
+    else:
+        print("ERROR: NOT A VALID FILE OR DIRECTORY!")
+        sys.exit(-1)
+    
+    fig = plt.figure()
+    ax = fig.subplots()
+    
+    def pltcur():
+        global current_file
+        print(current_file)
+        ax.clear()
+        ax.set_title(filedat[current_file][0])
+        ax.plot(filedat[current_file][1],filedat[current_file][2])
+        fig.canvas.draw()
+    
+    def next(val):
+        global current_file
+        current_file += 1
+        if current_file == len(filedat):
+            current_file = 0
+        pltcur()
+    
+    def prev(val):
+        global current_file
+        current_file -= 1 
+        if current_file == -1:
+            current_file += len(filedat)
+        pltcur()
+         
+    bnext = Button(plt.axes([0.0, 0.8, 0.1, 0.1]), 'Next')
+    bprev = Button(plt.axes([0.0, 0.6, 0.1, 0.1]), 'Prev')
+    bnext.on_clicked(next)
+    bprev.on_clicked(prev)
+    pltcur()
+    plt.gca().set_aspect('equal')
+    plt.show()
