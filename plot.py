c21e74eb2fb4de955e1b14c61c03d9d87e19261e
Nicole Swierstra <Swiersw@wwu.edu>
2026-05-11 20:50:00 -0700

    working

--- plot.py
+++ plot.py
@@ -5,19 +5,20 @@ import sys, math, os
 from os import listdir
 from os.path import isfile, join
 
-def get_points(fp : str, centerpoint) -> tuple[tuple[float, float], list[float], list[float], list[tuple[float, float, float]]]:
+def get_points(fp : str, centerpoint) -> tuple[tuple[float, float], list[int], list[float], list[float], list[tuple[float, float, float]]]:
     xvals = []
     yvals = []
     with open(fp) as f:
         channels = f.readline().split(",")
         try:
+            int_idx = next(i for i,d in enumerate(channels) if d.startswith("\"Interval"))
             Lat_index = next(i for i,d in enumerate(channels) if d.startswith("\"Latitude"))
             Lon_index = next(i for i,d in enumerate(channels) if d.startswith("\"Longitude"))
             ax_index = next(i for i,d in enumerate(channels) if d.startswith("\"AccelX"))
             ay_index = next(i for i,d in enumerate(channels) if d.startswith("\"AccelY"))
             az_index = next(i for i,d in enumerate(channels) if d.startswith("\"AccelZ"))
         except:
-            return None, 0, 0, None 
+            return None, None, 0, 0, None 
         
         data = f.readline().split(",")
         #print(data[Lat_index])
@@ -39,9 +40,12 @@ def get_points(fp : str, centerpoint) -> tuple[tuple[float, float], list[float],
         m_per_deg_lat = 111132.954 - 559.822 * math.cos(2 * lat_radians) + 1.175 * math.cos(4 * lat_radians)
         m_per_deg_lon = 111412.84 * math.cos(lat_radians) - 93.5 * math.cos(3 * lat_radians) 
 
+        interval = []
         accel_x = []
         accel_y = []
         accel_z = []
+        theta = -47.8 * math.pi / 180.0
+        i = 0
         while(1):
             line = f.readline()
             if line == '': break
@@ -49,22 +53,26 @@ def get_points(fp : str, centerpoint) -> tuple[tuple[float, float], list[float],
 
             if len(data) > 100:
                 if data[Lat_index] != '':
-                    lat_x = (float(data[Lat_index]) - centerpoint[0]) * m_per_deg_lat
-                    lon_x = (float(data[Lon_index]) - centerpoint[1]) * m_per_deg_lon
-
-                
-                    yvals.append(lat_x)
-                    xvals.append(lon_x)
+                    try:
+                        lat_x = (float(data[Lat_index]) - centerpoint[0]) * m_per_deg_lat
+                        lon_x = (float(data[Lon_index]) - centerpoint[1]) * m_per_deg_lon 
+                        yvals.append(lat_x)
+                        xvals.append(lon_x)
+                    except:
+                        print("bad data: " + data[Lat_index])
                 if data[ax_index] != '':
                     ax = float(data[ax_index])
                     ay = float(data[ay_index])
                     az = float(data[az_index])
                     
-                    accel_x.append(-0.685 * ay + 0.729 * az)
-                    accel_y.append(-ax)
-                    accel_z.append(-0.685 * az + -0.729 * ay)
+                    ct = math.cos(theta)
+                    st = math.sin(theta) 
+                    accel_x.append(ct * ax + st * az)
+                    accel_y.append(ay)
+                    accel_z.append(-st * ax + ct * az)
+                    interval.append(float(data[int_idx]) / 1000.0)
         
-    return (centerpoint, xvals, yvals, (accel_x, accel_y, accel_z))
+    return (centerpoint, interval, xvals, yvals, (accel_x, accel_y, accel_z))
 
 
 current_file = 1
@@ -73,19 +81,19 @@ if __name__ == '__main__':
     fp = sys.argv[1]
     filedat = []
     if (os.path.isfile(fp)):
-        centerpoint, xp, yp, a = get_points(fp, None)
-        filedat.append([fp, xp, yp, a]) 
+        centerpoint, interval, xp, yp, a = get_points(fp, None)
+        filedat.append([fp, xp, yp, a, interval]) 
     elif (os.path.isdir(fp)):
         # from stackoverflow lol
         logs = [f for f in listdir(fp) if (isfile(join(fp, f)) and f.endswith(".log"))]
         centerpoint = None
         for f in logs:
             print("reading " + join(fp, f))
-            centerpoint, xp, yp, a = get_points(join(fp, f), centerpoint)
+            centerpoint, interval, xp, yp, a = get_points(join(fp, f), centerpoint)
             if (centerpoint == None or len(xp) == 0):
                 print("Error reading file " + str(f))
             else: 
-                filedat.append([f, xp, yp, a]) 
+                filedat.append([f, xp, yp, a, interval]) 
     else:
         print("ERROR: NOT A VALID FILE OR DIRECTORY!")
         sys.exit(-1)
@@ -98,12 +106,16 @@ if __name__ == '__main__':
         ax.clear()
         ax.set_title(filedat[current_file][0])
         if (len(sys.argv) > 2):
-            if (sys.argv[2] == 'e'): 
-                ax.scatter(filedat[current_file][3][1],filedat[current_file][3][0])
+            if (sys.argv[2] == 'e'):
+                ax.plot(filedat[current_file][4], filedat[current_file][3][0], color='red')
+                ax.plot(filedat[current_file][4], filedat[current_file][3][1], color='green')
+                ax.plot(filedat[current_file][4], filedat[current_file][3][2], color='blue')
             else:
                 ax.plot(filedat[current_file][1],filedat[current_file][2])
         else:
             ax.plot(filedat[current_file][1],filedat[current_file][2])
+        
+        plt.gca().set_aspect('equal')
         fig.canvas.draw()
      
     def next(val):
